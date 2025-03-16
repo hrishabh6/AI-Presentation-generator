47b038f8ae5a5a87720bee78cad7237373a121ae
hrishabh6 <hrishabhjoshi123@gmail.com>
2025-03-16 20:24:45 +0530

    Theme preview animations

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,3 +1,4 @@
+
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
@@ -6,13 +7,13 @@ type Props = {
   children: React.ReactNode;
 };
 
-const Layout = async ({ children }: Props) => {
-  const { userId } = await auth();
-
+const Layout =  async ({ children }: Props) => {
+  
+  const {userId} = await auth();
   if (!userId) {
     redirect("/sign-in");
   }
-
+  
 
   return <div className="w-full min-h-screen">{children}</div>;
 };
