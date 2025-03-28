a4888847d3fca2c53db92cd943098e04cc478c5d
hrishabh6 <hrishabhjoshi123@gmail.com>
2025-03-28 13:28:01 +0530

    Recovery functions

--- src/actions/project.ts
+++ src/actions/project.ts
@@ -273,3 +273,32 @@ export const deleteAllProjects = async (projectIds: string[]) => {
     }
 
 } 
+
+export const getDeletedProjects = async () => {
+  try {
+    const checkUser = await onAuthenticateUser();
+    if (checkUser.status !== 200 || !checkUser.user) {
+      return { status: 403, error: "User not authenticated" };
+    }
+
+    const projects = await client.project.findMany({
+      where: {
+        userId: checkUser.user.id,
+        isDeleted: true,
+      },
+      orderBy: {
+        updatedAt: "desc",
+      },
+    });
+
+    if (projects.length === 0) {
+      return { status: 404, error: "No projects found" };
+    }
+
+    return { status: 200, data: projects}
+
+  } catch (error) {
+    console.log("❌ Error in getDeletedProjects", error);
+    return { status: 500, error: "Internal Server error" };
+  }
+}
--- src/app/(protected)/(pages)/(dashboardPages)/trash/page.tsx
+++ src/app/(protected)/(pages)/(dashboardPages)/trash/page.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import DeleteAllButton from './_components/DeleteAllButton'
+import { getDeletedProjects } from '@/actions/project'
+import NotFound from '@/components/global/not-found'
+import Projects from '@/components/global/projects'
 
 
 const Page = async () => {
 
+  const deletedProjects = await getDeletedProjects()
 
+  if (!deletedProjects.data) {
+    return <NotFound/>
+  }
 
   return (
+
+    
+
     <div className="flex flex-col gap-6 relative">
       <div className="flex justify-between items-center">
         <div className="flex flex-col items-start">
